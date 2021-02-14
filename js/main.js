7395a915904604167b8e80a10acca07816ba09df
Jon Jackson <jon.jackson@perficient.com>
2021-02-14 17:08:53 -0500

    dragging to top of container to change it. need different directions and better usability

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function getDragAfterElement(container, y) {
   return draggableElements.reduce(
     (closest, child) => {
       const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
+      const offset = y - box.top - box.height / 1.4; //was 2 at the end originally... changed it for sharper image positioning
       if (offset < 0 && offset > closest.offset) {
         return { offset: offset, element: child };
       } else {
